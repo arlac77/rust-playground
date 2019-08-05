deacd2ff6725d052a3a02551652a9da8bd2cb762
Markus Felten <Markus.Felten@db.com>
2019-08-05 19:09:08 +0200

    chore: initial checkin

---
+++ main.rs
@@ -0,0 +1,9 @@
+ fn main() {
+    let mut x = 5;
+    let y = &x;
+    //let z = &mut x; // This was not allowed before 1.31.0.
+    let z = &x; // This was not allowed before 1.31.0.
+ 
+    println!("x={} y={} z={}", x,y, z);
+ }
+
